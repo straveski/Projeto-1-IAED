de48cdf14ba0bb837a7b8c6625f06ad353ebff07
straveski <73653498+straveski@users.noreply.github.com>
2021-03-28 04:11:20 +0100

    Update proj.c

--- proj.c
+++ proj.c
@@ -15,14 +15,14 @@
 #define MAXUT 50
 
 /* Variaveis globais */
-global time;
-global tarefas;
+int time;
+int tarefas = 0;
 
 /* Estrutura */
 typedef struct{
     char desc[MAXDESC];  /*descricao*/
     char ativ[MAXATIV];  /*atividade*/
-    int duracao = 0;     /*duracao da tarefa*/
+    int dur;     /*duracao da tarefa*/
     int timeexec;        /*tempo em que saiu da tarefa inicial*/
 } Tarefa;
 
@@ -30,7 +30,7 @@ typedef struct{
 
 /* prototipos*/
 
-
+void comando_t(Tarefa dis[]);
 
 
 
@@ -38,11 +38,48 @@ typedef struct{
 
 int main(){
     Tarefa ids[MAXTAREFAS];
+    char comando;
+    do {
+        comando = getchar();
+        if(comando == 't'){
+            comando_t(ids);
+        }
+        else if(comando == 'l'){
+        }
+        else if(comando == 'n'){
+        }
+        else if(comando == 'u'){
+        }
+        else if(comando == 'm'){
+        }
+        else if(comando == 'd'){
+        }
+        else if(comando == 'a'){
+        }
 
-
-
-
-
+    } while(comando != 'q');
 
     return 0;
+}
+
+void comando_t(Tarefa ids[]){
+    int duracao, i;
+    char descricao[MAXDESC];
+    
+    scanf("%d %s", &duracao, descricao);
+    
+    for(i=0; i < tarefas-1; i++){
+        if (strcmp(descricao, ids[i].desc) == 0){
+            printf("duplicate description");
+            break;}
+    }
+    if (tarefas == MAXTAREFAS)
+        printf("too many tasks");
+
+    else{
+        ids[tarefas].dur = duracao;
+        strcpy(ids[tarefas].desc, descricao);
+        tarefas++;
+        printf("task %d\n", tarefas);
+    }
 }
